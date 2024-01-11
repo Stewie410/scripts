4d21357ce076a92c2d6a813d9b677c7a993875f5
Alex Paarfus <stewie410@gmail.com>
2024-01-11 17:51:45 -0500

    pull gcd() from lib if not defined

--- bash/lib/math/lcm.sh
+++ bash/lib/math/lcm.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
 lcm() {
-    _gcd() {
-        if (( $1 % $2 == 0 )); then
-            printf '%d\n' "${2}"
-            return
-        fi
-        _gcd "${2}" "$(( $1 % $2 ))"
-    }
-
     (( $# > 1 )) || return 1
+    declare -f gcd &>/dev/null || source "${BASH_LIB}/math/gcd.sh"
     local -i g l
     l="${1}"
     shift
     while (( $# > 0 )); do
-        g="$(_gcd "${l}" "${1}")"
+        g="$(gcd "${l}" "${1}")"
         l="$(( (l * $1) / g ))"
         shift
     done
