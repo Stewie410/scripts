9def96f192320f2c89dff7ec9139c2e7b744982e
Alex Paarfus <stewie410@gmail.com>
2024-01-12 09:17:08 -0500

    will include '_var' variants in same file

--- bash/lib/array/rev.sh
+++ bash/lib/array/rev.sh
@@ -9,3 +9,19 @@ rev() {
     rev "${@}"
     shopt -u extdebug
 }
+
+rev_var() {
+    (( $# )) || return 1
+    if shopt -q extdebug &>/dev/null; then
+        arr=("${BASH_ARGV[@]}")
+        return
+    fi
+    shopt -s extdebug
+    while (( $# > 0 )); do
+        # shellcheck disable=SC2178
+        local -n arr="${1}"
+        rev_var "${arr[@]}"
+        shift
+    done
+    shopt -u extdebug
+}
