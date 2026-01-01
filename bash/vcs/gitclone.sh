1b0e98ac2e0c106f500c68041d38d489bba7f50e
Alex Paarfus <stewie410@gmail.com>
2026-01-01 09:59:13 -0500

    fix(ux): bring to parity with local copy

--- bash/vcs/gitclone.sh
+++ bash/vcs/gitclone.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-#
-# Git-Clone wrapper
 
 show_help() {
     cat << EOF
@@ -10,7 +8,8 @@ USAGE: ${0##*/} [OPTIONS] SLUG [OUTDIR]
 
 OPTIONS:
     -h, --help          Show this help message
-    -g, --github        Pull from github (default)
+    -g, --github        Pull from ssh://git@github (default)
+    -G, --github-http   Pull from http://github.com
     -l, --gitlab        Pull from gitlab
     -s, --suckless      Pull from suckless
     -b, --base-url URL  Pull from custom URL
@@ -20,55 +19,44 @@ SLUG:
 EOF
 }
 
-require() {
-    command -v "${1}" &>/dev/null && return 0
-    printf 'Missing required application: %s\n' "${1}" >&2
-    return 1
-}
-
-is_online() {
-    ping -qc 1 '8.8.8.8' | grep --quiet --ignore-case 'unreachable' || return 0
-    printf 'Requires an internet connection\n' >&2
-    return 1
+is_slug() {
+    [[ "${1}" == *"/"* ]] || return 1
+    [[ "${1}" =~ .+/.+ ]] || return 1
+    return 0
 }
 
 main() {
-    local opts base default_base
+    local opts url
+    url="git@github.com:"
     opts="$(getopt \
-        --options hglsb: \
-        --longoptions help,github,gitlab,suckless,base-url: \
+        --options hgGlsb: \
+        --longoptions help,github,github-http,gitlab,suckless,base-url: \
         --name "${0##*/}" \
         -- "${@}" \
     )"
 
-    default_base='https://github.com'
-
     eval set -- "${opts}"
     while true; do
         case "${1}" in
-            -h | --help )       show_help; return 0;;
-            -g | --github )     base="${default_base}";;
-            -l | --lab )        base='https://gitlab.com';;
-            -s | --suckless )   base='https://git.suckless/org';;
-            -b | --base-url )   base="${2}"; shift;;
-            -- )                shift; break;;
-            * )                 break;;
+            -h | --help )           show_help; return 0;;
+            -g | --github )         url="git@github.com:";;
+            -G | --github-http )    url="https://github.com";;
+            -l | --gitlab )         url="https://gitlab.com";;
+            -s | --suckless )       url="https://git.suckless.org";;
+            -b | --base-url )       url="${2}"; shift;;
+            -- )                    shift; break;;
+            * )                     break;;
         esac
         shift
     done
 
-    require 'git' || return 1
-    is_online || return 1
-
-    if [[ -z "${1}" ]]; then
-        printf 'No slug specified\n' >&2
-        return 1
-    elif [[ "${1}" != */* ]]; then
-        printf 'Invalid slug format' >&2
+    if ! is_slug "${1}"; then
+        printf '\e[0;31mERROR\e[0m: Must provide valid USER/REPO slug!\n' >&2
         return 1
     fi
 
-    git clone "${base:-${default_base}}/${1}.git" "${2:-${PWD}/${1##*/}}"
+    set -- "${url}/${1}.git" "${@:2}"
+    git clone "${@}"
 }
 
 main "${@}"
